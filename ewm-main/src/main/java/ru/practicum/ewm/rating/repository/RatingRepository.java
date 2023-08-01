276e8b1410f34936de1ae5f1685ab569875d9317
TemaJava <101995680+TemaJava@users.noreply.github.com>
2023-08-01 16:47:40 +0300

    Update RatingRepository.java

--- ewm-main/src/main/java/ru/practicum/ewm/rating/repository/RatingRepository.java
+++ ewm-main/src/main/java/ru/practicum/ewm/rating/repository/RatingRepository.java
@@ -7,7 +7,10 @@ import ru.practicum.ewm.rating.model.Rating;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+ 
     Optional<Rating> findByIdAndUserId(Long ratingId, Long userId);
+    
     List<Rating> findAllByEventIdIn(@Param("eventId") List<Long> eventId);
 }
